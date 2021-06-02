5f77728a5cd7d109ece45a2efb74cc82f4ce14da
Anthony Ortega <50421374+Anthony9811@users.noreply.github.com>
2021-06-02 13:48:08 -0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: colors,
+    },
+  },
+  variants: {
+    extend: {
+      borderWidth: ['last'],
+      margin: ['last'],
+    }
+  },
+  plugins: [],
+}
